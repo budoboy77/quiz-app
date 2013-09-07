7fe2fb3855c9a5a16ce3728b171e0c0d26e4ec1f
Bryan <bew@bryanewhite.com>
2013-09-07 14:42:59 -0600

    Adjusted models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ActiveRecord::Base
+  belongs_to :category
+  belongs_to :type
   has_and_belongs_to_many :quiz_setups, join_table: "questions_quiz_setups",
   class_name: "QuizSetup", foreign_key: "question_id",
   association_foreign_key: "quiz_setup_id"
